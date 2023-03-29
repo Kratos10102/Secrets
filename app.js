4ccd36b3202fed8aef9d1b8a49a1171711b7bac7
Daniel <danielpavlova101@gmail.com>
2023-03-29 20:44:11 +0300

    some changes

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 const SECRET_KEY = process.env.SECRET_KEY;
 // Using environment variables ^ 
-const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
-const app = express()
 const { Schema } = mongoose;
 
 const LocalStrategy = require('passport-local').Strategy;
@@ -15,31 +13,46 @@ const passport = require('passport');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 // Including encryption ^
-
+const express = require("express")
 const session = require('express-session');
+const app = express()
+
 app.use(express.static("public"))
 app.set('view engine', 'ejs')
 
-mongoose.connect(process.env.CONNECT)
-
 app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect(process.env.CONNECT);
+
+
+app.use(session({
+  secret: SECRET_KEY,
+  resave: false,
+  saveUninitialized: true
+}));
+
+
 passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ email: username }, function (err, user) {
-      if (err) { return done(err); }
+  async function(username, password, done) {
+    try {
+      const user = await User.findOne({ email: username });
       if (!user) { return done(null, false); }
-      if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) { return done(null, false); }
       return done(null, user);
-    });
+    } catch (err) {
+      return done(err);
+    }
   }
 ));
+
 
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
+passport.deserializeUser(async function(id, done) {
   User.findById(id, function(err, user) {
     done(err, user);
   });
@@ -77,10 +90,12 @@ app.get("/secrets", function(req, res){
 
 
 app.post("/register", async function(req,res){
+  mongoose.connect(process.env.CONNECT);
   const newUser = new User({
       email: req.body.username,
       password: await bcrypt.hash(req.body.password, saltRounds)
     })
+
    try {
     await newUser.save()
     res.redirect("/secrets")
@@ -90,12 +105,14 @@ app.post("/register", async function(req,res){
 })
 
 
+app.post('/login',passport.authenticate('local', {
+  successRedirect: '/secrets',
+  failureRedirect: '/login'
+}));
 
-app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/secrets');
-  });
+
+
+
 
   
 
@@ -104,3 +121,5 @@ app.post('/login',
 app.listen(3000, function() {
     console.log("Server started on port 3000")
 })
+
+
